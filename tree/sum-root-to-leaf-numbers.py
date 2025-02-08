b154656a1dd9b3c48f30660c08f5b8f34260903a
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-07 23:57:52 -0500

    LC-sum-root-to-leaf-numbers [Runtime: N/A; Memory: N/A]

---
+++ tree/sum-root-to-leaf-numbers.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        paths = []
+        res = []
+        def dfs(node):
+            if not node:
+                return 
+            res.append(node.val)
+            if not node.left and not node.right:
+                paths.append(res[:])
+                return paths
+            dfs(node.left)
+            res.pop()
+            dfs(node.right)
+            res.pop()
+            return paths
+        sum_ = 0
+        for l in dfs(root):
+            sum_ += int("".join(map(str, l)))
+        return sum_
