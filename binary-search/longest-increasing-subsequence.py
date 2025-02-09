931a41cad77deec10864beac11b1941f561763c6
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-09 17:58:36 -0500

    LC-longest-increasing-subsequence [Runtime: 41 ms; Memory: 54.1 MB]

--- binary-search/longest-increasing-subsequence.py
+++ binary-search/longest-increasing-subsequence.py
@@ -1,23 +1,14 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        def insert(arr, x, start, end):
-            if x < arr[0]:
-                return [x]
-            if x == arr[0]:
-                return arr
-            if start == end:
-                return arr[:start] + [x]
-            mid = (start + end) // 2
-    
-            if x > arr[mid]:
-                return insert(arr, x, mid + 1, end)
-            elif x < arr[mid]:
-                return insert(arr, x, start, end-1)
+        
         n = len(nums)
         dp = [[] for _ in range(n)]
         dp[0] = [nums[0]]
         for i in range(1, n):
-            dp[i] = insert(dp[i-1], nums[i], 0, len(dp[i-1]))
-        return max(len(d) for d in dp)
+            idx = bisect.bisect_left(dp[i-1], nums[i])
+            dp[i] = dp[i-1][:idx] + [nums[i]] + dp[i-1][idx+1:]
+            
+            
+        return len(dp[-1])
             
             
