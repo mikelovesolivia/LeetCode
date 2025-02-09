ed934a244a4251ba44ac724a0ea7485ce1584f16
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-08 19:47:05 -0500

    LC-house-robber-iii [Runtime: N/A; Memory: N/A]

---
+++ binary-tree/house-robber-iii.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        memo = {}
+        def dfs(node):
+            if not node:
+                return 0
+            if node in memo:
+                return memo[node]
+            val = node.val
+            if node.left:
+                val += dfs(node.left.left) + dfs(node.left.right)
+            if node.right:
+                val += dfs(node.right.left) + dfs(node.right.right)
+            memo[node] = val
+            return val
+        return max(dfs(root), dfs(root.left) + dfs(root.right))
+                
