141516e097116abb9aaccee8ff7dd272e33f0e19
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-09 18:07:02 -0500

    LC-longest-increasing-subsequence [Runtime: 2 ms; Memory: 18.2 MB]

--- array/longest-increasing-subsequence.py
+++ array/longest-increasing-subsequence.py
@@ -2,13 +2,13 @@ class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         
         n = len(nums)
-        dp = [[] for _ in range(n)]
-        dp[0] = [nums[0]]
-        for i in range(1, n):
-            idx = bisect.bisect_left(dp[i-1], nums[i])
-            dp[i] = dp[i-1][:idx] + [nums[i]] + dp[i-1][idx+1:]
-            
-            
-        return len(dp[-1])
+        dp = []
+        for i in range(n):
+            idx = bisect.bisect_left(dp, nums[i])
+            if idx == len(dp):
+                dp.append(nums[i])
+            else:
+                dp[idx] = nums[i]
+        return len(dp)
             
             
