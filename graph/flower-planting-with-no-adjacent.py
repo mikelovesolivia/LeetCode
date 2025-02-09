bf3e4969bc1ca5000a2e4d7b6bb81b11ce8faee6
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-09 10:39:17 -0500

    LC-flower-planting-with-no-adjacent [Runtime: N/A; Memory: N/A]

---
+++ graph/flower-planting-with-no-adjacent.py
@@ -0,0 +1,29 @@
+class Solution:
+    def gardenNoAdj(self, n: int, paths: List[List[int]]) -> List[int]:
+        graph = [[] for _ in range(n)]
+        for i, j in paths:
+            graph[i-1].append(j-1)
+            graph[j-1].append(i-1)
+        visited = [0] * n
+        colors = [0] * n
+        colors[0] = 0
+        queue = [0]
+        while queue:
+            node = queue.pop(0)
+            visited[node] = 1
+            color_used = [0] * 4
+            for neighbor in graph[node]:
+                if visited[neighbor]:
+                    nbr_color = colors[neighbor]
+                    color_used[nbr_color] = 1
+            for c, used in enumerate(color_used):
+                if not used:
+                    color = c 
+                    break
+            colors[node] = color
+            visited[node] = 1
+            for neighbor in graph[node]:
+                if not visited[neighbor]:
+                    queue.append(neighbor)
+        return [x + 1 for x in colors]
+                
