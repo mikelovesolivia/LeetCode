a407c4bf0b707cd81516175eeb7fdf7154f26bf7
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-09 10:47:55 -0500

    LC-flower-planting-with-no-adjacent [Runtime: 5151 ms; Memory: 23.7 MB]

--- graph/flower-planting-with-no-adjacent.py
+++ graph/flower-planting-with-no-adjacent.py
@@ -7,23 +7,27 @@ class Solution:
         visited = [0] * n
         colors = [0] * n
         colors[0] = 0
-        queue = [0]
-        while queue:
-            node = queue.pop(0)
-            visited[node] = 1
-            color_used = [0] * 4
-            for neighbor in graph[node]:
-                if visited[neighbor]:
-                    nbr_color = colors[neighbor]
-                    color_used[nbr_color] = 1
-            for c, used in enumerate(color_used):
-                if not used:
-                    color = c 
-                    break
-            colors[node] = color
-            visited[node] = 1
-            for neighbor in graph[node]:
-                if not visited[neighbor]:
-                    queue.append(neighbor)
+        def bfs(node_selected):
+            queue = [node_selected]
+            while queue:
+                node = queue.pop(0)
+                visited[node] = 1
+                color_used = [0] * 4
+                for neighbor in graph[node]:
+                    if visited[neighbor]:
+                        nbr_color = colors[neighbor]
+                        color_used[nbr_color] = 1
+                for c, used in enumerate(color_used):
+                    if not used:
+                        color = c 
+                        break
+                colors[node] = color
+                visited[node] = 1
+                for neighbor in graph[node]:
+                    if not visited[neighbor]:
+                        queue.append(neighbor)
+        for node in range(n):
+            if not visited[node]:
+                bfs(node)
         return [x + 1 for x in colors]
                 
