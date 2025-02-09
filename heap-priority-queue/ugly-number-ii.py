5929f496a536b96c2c34ffac42fca8b2c0b420b4
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-09 16:23:32 -0500

    LC-ugly-number-ii [Runtime: 87 ms; Memory: 17.9 MB]

--- heap-priority-queue/ugly-number-ii.py
+++ heap-priority-queue/ugly-number-ii.py
@@ -1,12 +1,13 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
         heap = [1]
-        seq = []
-        while len(seq) < n:
+        seen = set()
+        for i in range(n):
             x = heapq.heappop(heap)
             for mul in (2, 3, 5):
                 y = mul * x
-                heapq.heappush(heap, y)
-            if x not in seq:
-                heapq.heappush(seq, x)
-        return seq[n-1]
+                if y not in seen:
+                    heapq.heappush(heap, y)
+                    seen.add(y)
+         
+        return x
