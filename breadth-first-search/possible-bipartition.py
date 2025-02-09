68f44a51522940890033f42d3c7ee47e96afddc8
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-09 14:52:41 -0500

    LC-possible-bipartition [Runtime: N/A; Memory: N/A]

---
+++ breadth-first-search/possible-bipartition.py
@@ -0,0 +1,20 @@
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        graph = [[] for _ in range(n)]
+        for i, j in dislikes:
+            graph[i-1].append(j-1)
+            graph[j-1].append(i-1)
+        
+        color = [-1] * n
+        queue = [0]
+        while queue:
+            node = queue.pop(0)
+            for neighbor in graph[node]:
+                if color[neighbor] == -1:
+                    color[neighbor] = 1 - color[node]
+                    queue.append(neighbor)
+                else:
+                    if color[neighbor] == color[node]:
+                        return False
+        return True
+        
