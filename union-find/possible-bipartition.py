a6e3852cd79d7969809e4be5927b56525916222a
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-09 15:24:34 -0500

    LC-possible-bipartition [Runtime: N/A; Memory: N/A]

--- union-find/possible-bipartition.py
+++ union-find/possible-bipartition.py
@@ -4,22 +4,23 @@ class Solution:
         for i, j in dislikes:
             graph[i-1].append(j-1)
             graph[j-1].append(i-1)
-        
         color = [-1] * n
-        def bfs(root):
-            queue = [root]
-            color[root] = 0
+
+        for i in range(n):
+            if color[i] != -1:
+                continue
+            queue = [(0, 0)]
+            color[i] = 0
             while queue:
-                node = queue.pop(0)
+                node, c = queue.pop(0)
                 for neighbor in graph[node]:
                     if color[neighbor] == -1:
-                        color[neighbor] = 1 - color[node]
-                        queue.append(neighbor)
+                        color[neighbor] = 1 - c
+                        queue.append((neighbor, color[neighbor]))
                     else:
-                        if color[neighbor] == color[node]:
+                        if color[neighbor] == c:
                             return False
-        for i in range(n):
-            if color[i] != -1 and not bfs(i):
-                return False
-        return True
+            return True
+
+        
         
