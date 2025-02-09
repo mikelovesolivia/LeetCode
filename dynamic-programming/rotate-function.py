d28024624cdbfc811d88ca236117c1527a018c08
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-09 18:40:58 -0500

    LC-rotate-function [Runtime: 167 ms; Memory: 27.4 MB]

--- dynamic-programming/rotate-function.py
+++ dynamic-programming/rotate-function.py
@@ -3,6 +3,7 @@ class Solution:
         n = len(nums)
         f = [0] * n
         f[0] = sum(i * num for i, num in enumerate(nums))
+        sum_ = sum(nums)
         for i in range(1, n):
-            f[i] = f[i-1] + sum(nums) - n * nums[n-i]
+            f[i] = f[i-1] + sum_ - n * nums[n-i]
         return max(f)
