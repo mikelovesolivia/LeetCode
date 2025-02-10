917a337a3f43af352d10d53a93d0e4a260f8206c
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-10 10:21:48 -0500

    LC-ways-to-express-an-integer-as-sum-of-powers [Runtime: N/A; Memory: N/A]

--- dynamic-programming/ways-to-express-an-integer-as-sum-of-powers.py
+++ dynamic-programming/ways-to-express-an-integer-as-sum-of-powers.py
@@ -5,7 +5,8 @@ class Solution:
         m = len(weights)
         dp = [0] * (n + 1)
         dp[0] = 1
+        MOD = 1e9 + 7
         for i in range(m):
             for j in range(n, weights[i]-1, -1):
-                dp[j] += dp[j-weights[i]]
-        return dp[-1]
+                dp[j] += dp[j-weights[i]] % MOD
+        return dp[-1] % MOD
