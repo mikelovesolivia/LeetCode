552552e59b1b77fe976065774c95623576fb0eb2
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-10 18:44:39 -0500

    LC-find-largest-value-in-each-tree-row [Runtime: N/A; Memory: N/A]

---
+++ breadth-first-search/find-largest-value-in-each-tree-row.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        queue = [(root, 0)]
+        max_vals = collections.defaultdict(int)
+        while queue:
+            node, level = queue.pop(0)
+            max_vals[level] = max(max_vals.get(level, 0), node.val)
+            if node.left:
+                queue.append((node.left, level + 1))
+            if node.right:
+                queue.append((node.right, level + 1))
+        return [v for v in dict(sorted(max_vals.items())).values()]
+            
+
+        
