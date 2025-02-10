c01fde301ea1b83e36314a4affcbdb0a1ce80109
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-10 00:05:41 -0500

    LC-maximum-length-of-pair-chain [Runtime: N/A; Memory: N/A]

---
+++ greedy/maximum-length-of-pair-chain.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findLongestChain(self, pairs: List[List[int]]) -> int:
+        pairs,sort(key=lambda x: x[1])
+        prev_end = - float('inf')
+        count = 0
+        for l, r in pairs:
+            if l > prev_end:
+                prev_end = r
+                count += 1
+        return count
