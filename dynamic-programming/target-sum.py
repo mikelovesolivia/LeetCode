03e7afbcab54f8acf28752afa63a90121c59c3e5
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-10 10:39:00 -0500

    LC-target-sum [Runtime: N/A; Memory: N/A]

--- dynamic-programming/target-sum.py
+++ dynamic-programming/target-sum.py
@@ -2,6 +2,8 @@ class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         n = len(nums)
         sum_ = sum(nums)
+        if (sum_ + target) % 2 == 1:
+            return 0
         W = (sum_ + target) // 2
         dp = [0] * (W+1)
         dp[0] = 1
