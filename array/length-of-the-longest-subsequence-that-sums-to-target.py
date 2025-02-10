fdb9799650d17acc296e21c57d3fbf6ab971b1a2
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-09 20:24:05 -0500

    LC-length-of-the-longest-subsequence-that-sums-to-target [Runtime: 2174 ms; Memory: 17.8 MB]

--- array/length-of-the-longest-subsequence-that-sums-to-target.py
+++ array/length-of-the-longest-subsequence-that-sums-to-target.py
@@ -1,9 +1,9 @@
 class Solution:
     def lengthOfLongestSubsequence(self, nums: List[int], target: int) -> int:
-        dp = [0] * (target + 1)
+        dp = [-float('inf')] * (target + 1)
         dp[0] = 0
         n = len(nums)
         for i in range(n):
             for j in range(target, nums[i]-1, -1):
                 dp[j] = max(dp[j], dp[j-nums[i]] + 1)
-        return dp[-1]
+        return dp[-1] if dp[-1] != -float('inf') else -1
