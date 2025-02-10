136569b264e84dc5ea35c1330a4443dfa7fb05f4
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-10 08:44:36 -0500

    LC-perfect-squares [Runtime: 2273 ms; Memory: 18.1 MB]

--- dynamic-programming/perfect-squares.py
+++ dynamic-programming/perfect-squares.py
@@ -1,11 +1,11 @@
 class Solution:
     def numSquares(self, n: int) -> int:
-        V = int(math.sqrt(n)) + 1
-        values = [i ** 2 for i in range(1, V)]
-        m = len(values)
+        W = int(math.sqrt(n)) + 1
+        weights = [i ** 2 for i in range(1, W)]
+        m = len(weights)
         dp = [float('inf')] * (n+1)
         dp[0] = 0
         for i in range(m):
-            for j in range(values[i], n+1):
-                dp[j] = min(dp[j], dp[j-values[i]]+1)
+            for j in range(weights[i], n+1):
+                dp[j] = min(dp[j], dp[j-weights[i]]+1)
         return dp[-1]
