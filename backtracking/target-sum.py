b81db8d6f975d53cd8710f87b26f4e055fcd91f6
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-10 10:38:28 -0500

    LC-target-sum [Runtime: N/A; Memory: N/A]

--- backtracking/target-sum.py
+++ backtracking/target-sum.py
@@ -1,11 +1,11 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         n = len(nums)
-        def dfs(i, j):
-            if i == n:
-                if j == target:
-                    return 1
-                else:
-                    return 0
-            return dfs(i+1, j+nums[i]) + dfs(i+1, j-nums[i])
-        return dfs(0, 0)
+        sum_ = sum(nums)
+        W = (sum_ + target) // 2
+        dp = [0] * (W+1)
+        dp[0] = 1
+        for i in range(n):
+            for j in range(W, nums[i]-1, -1):
+                dp[j] += dp[j-nums[i]]
+        return dp[-1]
