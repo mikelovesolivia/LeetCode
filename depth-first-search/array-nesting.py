116cfbcdeba54ed895a7d99f5c45661c2549600e
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-10 19:15:04 -0500

    LC-array-nesting [Runtime: N/A; Memory: N/A]

--- depth-first-search/array-nesting.py
+++ depth-first-search/array-nesting.py
@@ -1,12 +1,17 @@
 class Solution:
     def arrayNesting(self, nums: List[int]) -> int:
         s = collections.defaultdict(set)
-
+        memo = {}
         def dfs(num, start):
+            if num in memo:
+                return memo[num]
             if num in s[start]:
-                return len(s[start])
+                return 0
             s[start].add(num)
-            return dfs(nums[num], start)
+            length = 1
+            length += dfs(nums[num], start)
+            memo[start] = length
+            return length
         max_len = 0
         for i, num in enumerate(nums):
             max_len = max(max_len, dfs(num, i))
