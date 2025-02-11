2b0255c9f6ead96e30510588c99329c0c2dc2f25
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-10 19:24:51 -0500

    LC-array-nesting [Runtime: 187 ms; Memory: 67.1 MB]

--- array/array-nesting.py
+++ array/array-nesting.py
@@ -1,18 +1,13 @@
 class Solution:
     def arrayNesting(self, nums: List[int]) -> int:
-        s = collections.defaultdict(set)
-        memo = {}
-        def dfs(num, start):
-            if num in memo:
-                return memo[num]
-            if num in s[start]:
+        visited = [False] * len(nums)
+        def dfs(i):
+            if not visited[i]:
+                visited[i] = True
+                return 1 + dfs(nums[i])
+            else:
                 return 0
-            s[start].add(num)
-            length = 1
-            length += dfs(nums[num], start)
-            return length
         max_len = 0
         for i, num in enumerate(nums):
-            memo[num] = dfs(num, i)
-            max_len = max(max_len, memo[num])
-        return (max_len + 1) // 2
+            max_len = max(max_len, dfs(i))
+        return max_len 
