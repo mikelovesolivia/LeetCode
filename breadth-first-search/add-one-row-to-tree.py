4ce82582b4865e3cb218c20ea2478a878df4af92
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-10 19:47:46 -0500

    LC-add-one-row-to-tree [Runtime: N/A; Memory: N/A]

---
+++ breadth-first-search/add-one-row-to-tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def addOneRow(self, root: Optional[TreeNode], val: int, depth: int) -> Optional[TreeNode]:
+        queue = [(root, 1)]
+        while queue:
+            node, level = queue.pop(0)
+            if level == depth - 1:
+                left = node.left
+                right = node.right
+                node.left = TreeNode(val=val, left=left, right=None)
+                node.right = TreeNode(val=val, left=None, right=right) 
+
+            if level >= depth:
+                break
+            if node.left:
+                queue.append((node.left, level + 1))
+            if node.right:
+                queue.append((node.right, level + 1))
+        return root
