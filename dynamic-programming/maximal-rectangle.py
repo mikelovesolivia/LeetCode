844e443d18799ce04b7394f8617498af2156c292
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-12 17:34:22 -0500

    LC-maximal-rectangle [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/maximal-rectangle.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        n = len(matrix)
+        m = len(matrix[0])
+        dp = [[[0, 0] for _ in range(m)] for _ in range(n)]
+        ans = 0
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] == "1":
+                    if i == 0:
+                        if j == 0:
+                            dp[i][j] = [1, 1]
+                        else:
+                            dp[i][j] = [1, dp[i][j - 1][1] + 1]
+                    elif j == 0:
+                        dp[i][j] = [dp[i - 1][j][0] + 1, 1]
+                    else:
+                        dp[i][j] = [
+                            min(dp[i - 1][j][0], dp[i - 1][j - 1][0]) + 1,
+                            min(dp[i][j - 1][1], dp[i - 1][j - 1][1]) + 1,
+                        ]
+                    ans = max(ans, dp[i][j][0] * dp[i][j][1])
+        return ans
