4897a62397fae43a3f9da676e560eada4ee43c3b
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-13 12:04:29 -0500

    LC-largest-number [Runtime: 3 ms; Memory: 17.8 MB]

--- string/largest-number.py
+++ string/largest-number.py
@@ -1,3 +1,6 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        n = len(nums)
+        num_str = [str(num) for num in nums]
+        num_str.sort(key=functools.cmp_to_key(lambda a, b: 1 if a+b>b+a else -1), reverse=True)
+        res = "".join(num_str)
+        return "0" if res[0]=="0" else res
