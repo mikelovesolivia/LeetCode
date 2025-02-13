031bffbd1e460ec22d9016f62763ea793c451c4a
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-12 20:17:55 -0500

    LC-count-nodes-equal-to-average-of-subtree [Runtime: 44 ms; Memory: 18 MB]

--- binary-tree/count-nodes-equal-to-average-of-subtree.py
+++ binary-tree/count-nodes-equal-to-average-of-subtree.py
@@ -6,7 +6,7 @@
 #         self.right = right
 class Solution:
     def averageOfSubtree(self, root: TreeNode) -> int:
-        
+        ans = 0
         def calcSum(node):
             if not node:
                 return 0, 0
@@ -14,17 +14,10 @@ class Solution:
             right_sum, right_count = calcSum(node.right)
             sum_ = left_sum + right_sum + node.val
             count = left_count + right_count + 1
+            avg = sum_ // count
+            if avg == node.val:
+                nonlocal ans
+                ans += 1
             return sum_, count 
-
-        def dfs(root):
-            if not root:
-                return 0
-            count = 0
-            sub_sum, sub_count = calcSum(root)
-            avg = sub_sum // sub_count if sub_sum else 0
-            if avg == root.val:
-                count += 1
-            count += dfs(root.left) + dfs(root.right)
-            return count
-        
-        return dfs(root)
+        calcSum(root)
+        return ans
