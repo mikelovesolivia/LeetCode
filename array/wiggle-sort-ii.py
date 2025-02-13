aa85c0a14067e21f63085491202d3b28777a24e9
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-13 14:17:44 -0500

    LC-wiggle-sort-ii [Runtime: 4 ms; Memory: 19.1 MB]

--- array/wiggle-sort-ii.py
+++ array/wiggle-sort-ii.py
@@ -4,6 +4,6 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         nums.sort()
-        half = len(nums) // 2
-        (nums[::2], nums[1::2]) = (nums[:half], nums[half:]) if len(nums)%2==0 else (nums[:half+1], nums[half+1:])
+        half = len(nums[::2])
+        nums[::2], nums[1::2] = nums[:half][::-1], nums[half:][::-1]
 
