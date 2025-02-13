69d3a19c07efe22338878a15b1b53feccbbb05ca
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-12 20:39:18 -0500

    LC-minimum-operations-to-exceed-threshold-value-ii [Runtime: 279 ms; Memory: 35.5 MB]

--- heap-priority-queue/minimum-operations-to-exceed-threshold-value-ii.py
+++ heap-priority-queue/minimum-operations-to-exceed-threshold-value-ii.py
@@ -1,10 +1,13 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
+        heapq.heapify(nums)
         x = heapq.heappop(nums)
         y = heapq.heappop(nums)
+        
         ops = 0
+        temp = []
         while x < k:
-            heapq.heappush(nums, 2*x+y)
+            heapq.heappush(nums, 2*min(x, y)+max(x, y))
             ops += 1
             if len(nums) >= 2:
                 x = heapq.heappop(nums)
