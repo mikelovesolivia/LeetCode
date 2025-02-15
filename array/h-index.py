8baf05a98a84f2892c9cbb2d7843ff97949d7fe2
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-15 16:50:02 -0500

    LC-h-index [Runtime: 0 ms; Memory: 18 MB]

--- array/h-index.py
+++ array/h-index.py
@@ -2,9 +2,8 @@ class Solution:
     def hIndex(self, citations: List[int]) -> int:
         citations.sort()
         n = len(citations)
-        h = 0
         for i in range(n):
-            if citations[i] <= n-i:
-                h = citations[i]
-        return h
+            if citations[i]>=n-i:
+                return n-i
+        return 0
         
