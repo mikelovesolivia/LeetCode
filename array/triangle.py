d96b735c425a38353597f766485b41481111cfbf
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-14 19:57:26 -0500

    LC-triangle [Runtime: 7 ms; Memory: 19.9 MB]

--- array/triangle.py
+++ array/triangle.py
@@ -2,5 +2,16 @@ class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n = len(triangle)
         m = len(triangle[-1])
-        dp = [0] * n
-        dp[0] = triangle[0]
+        dp = [[0 for _ in range(m)] for _ in range(n)]
+        dp[0][0] = triangle[0][0]
+        for i in range(1, n):
+            row = triangle[i]
+            l = len(row)
+            for j in range(l):
+                if j>0 and j<l-1:
+                    dp[i][j] = min(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
+                elif j==0:
+                    dp[i][j] = dp[i-1][j] + triangle[i][j]
+                elif j==l-1:
+                    dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+        return min(dp[-1])
