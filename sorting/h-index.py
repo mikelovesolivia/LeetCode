9ff42b135e6540a0763c43ab4a68946be7e5f606
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-15 16:42:28 -0500

    LC-h-index [Runtime: N/A; Memory: N/A]

--- sorting/h-index.py
+++ sorting/h-index.py
@@ -5,6 +5,6 @@ class Solution:
         h = 0
         for i in range(n):
             if citations[i] <= n-i:
-                h = citations[i]
+                h = min(n, citations[i])
         return h
         
