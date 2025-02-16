934922c5f2b136cdadc3bb2067424ba86f0b5423
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-16 16:24:03 -0500

    LC-edit-distance [Runtime: 71 ms; Memory: 21.1 MB]

--- string/edit-distance.py
+++ string/edit-distance.py
@@ -4,26 +4,16 @@ class Solution:
             return max(len(word1), len(word2))
         n = len(word1)
         m = len(word2)
-        dp = [[0 for _ in range(m)] for _ in range(n)]
+        dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
         # dp[i][j] distance needed to change word1[:i+1] to word2[:j+1]
-        start = 1
-        for i in range(n):
-            if i < n and i < m and word1[:i+1]==word2[:i+1]:
-                dp[i][0] = 0
-            else:
-                dp[i][0] = start
-                start += 1
-        start = 1
-        for j in range(m):
-            if j < n and j < m and word1[:j+1]==word2[:j+1]:
-                dp[0][j] = 0
-            else:
-                dp[0][j] = start
-                start += 1
-        for i in range(1, n):
-            for j in range(1, m):
-                if word1[i] != word2[j]:
+        for i in range(1, n+1):
+            dp[i][0] = i
+        for j in range(1, m+1):
+            dp[0][j] = j
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                if word1[i-1] != word2[j-1]:
                     dp[i][j] = min(dp[i-1][j]+1, dp[i][j-1]+1, dp[i-1][j-1] + 1)
                 else:
-                    dp[i][j] = min(dp[i-1][j]+1, dp[i][j-1]+1, dp[i-1][j-1])
+                    dp[i][j] = dp[i-1][j-1]
         return dp[-1][-1]
