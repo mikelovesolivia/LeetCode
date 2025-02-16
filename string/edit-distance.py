1180ddd6050ab2b2c3793286631f11a5b11b2d91
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-15 19:23:34 -0500

    LC-edit-distance [Runtime: N/A; Memory: N/A]

--- string/edit-distance.py
+++ string/edit-distance.py
@@ -1,7 +1,7 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        if not word1:
-            return len(word2)
+        if not word1 or not word2:
+            return max(len(word1), len(word2))
         n = len(word1)
         m = len(word2)
         dp = [[0 for _ in range(m)] for _ in range(n)]
