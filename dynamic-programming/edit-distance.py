e5ad3cc1bab5926b63ddbc80983d1e5d63a673b9
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-15 19:29:18 -0500

    LC-edit-distance [Runtime: N/A; Memory: N/A]

--- dynamic-programming/edit-distance.py
+++ dynamic-programming/edit-distance.py
@@ -6,10 +6,20 @@ class Solution:
         m = len(word2)
         dp = [[0 for _ in range(m)] for _ in range(n)]
         # dp[i][j] distance needed to change word1[:i+1] to word2[:j+1]
+        start = 1
         for i in range(n):
-            dp[i][0] = i+1
+            if i < n and i < m and word1[:i+1]==word2[:i+1]:
+                dp[i][0] = 0
+            else:
+                dp[i][0] = start
+                start += 1
+        start = 1
         for j in range(m):
-            dp[0][j] = j+1
+            if j < n and j < m and word1[:j+1]==word2[:j+1]:
+                dp[0][j] = 0
+            else:
+                dp[0][j] = start
+                start += 1
         for i in range(1, n):
             for j in range(1, m):
                 if word1[i] != word2[j]:
