7551708ade3217100e6132fcca38a99940e6f0f7
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-16 17:10:47 -0500

    LC-longest-palindromic-substring [Runtime: 2463 ms; Memory: 26.3 MB]

--- string/longest-palindromic-substring.py
+++ string/longest-palindromic-substring.py
@@ -5,18 +5,19 @@ class Solution:
         # dp[i][j]: whether s[i...j] is a palinfromic string
         res = ""
         max_len = 0
-        for i in range(n):
-            dp[i][i] = True
-            res=s[i]
-            max_len=1
-        for i in range(n-1):
-            for j in range(i+1, n):
-                if s[i] == s[j]:
+        for i in range(n-1,-1,-1):
+            for j in range(i, n):
+                if i==j:
+                    dp[i][j] = True
+                    if max_len<1:
+                        max_len=1
+                        res=s[i]
+                elif s[i] == s[j]:
                     if j==i+1:
                         dp[i][j] = True
                     else:
                         dp[i][j] = dp[i+1][j-1]
-                    if dp[i][j] and j-i+1 >= max_len:
+                    if dp[i][j] and j-i+1 > max_len:
                         max_len = j-i+1
                         res=s[i:j+1]
         return res
