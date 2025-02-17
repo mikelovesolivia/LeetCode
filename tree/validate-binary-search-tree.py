58bd373139f1cac345b558903c4ee186120f0d13
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-16 19:55:54 -0500

    LC-validate-binary-search-tree [Runtime: N/A; Memory: N/A]

---
+++ tree/validate-binary-search-tree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def dfs(node):
+            if not node:
+                return True
+            if node.left and node.left.val>node.val:
+                return False
+            if node.right and node.right.val<node.val:
+                return False
+            return True and dfs(node.left) and dfs(node.right)
+        return dfs(root)
