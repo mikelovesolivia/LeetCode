2b2d98a154419980655912e97ea5dc7fc7962a37
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-17 13:53:24 -0500

    LC-letter-tile-possibilities [Runtime: N/A; Memory: N/A]

---
+++ string/letter-tile-possibilities.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+        count = collections.Counter(tiles)
+        def backtrack():
+            total = 0
+            for char in count:
+                if count[char] > 0:
+                    count[char] -= 1
+                    total += 1 + backtrack()
+                    count[char] += 1
+            return count
+        return backtrack()
