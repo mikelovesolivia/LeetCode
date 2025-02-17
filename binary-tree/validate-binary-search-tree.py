6f87b6d5170e9810128466ac4d478b4151904840
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-16 23:13:55 -0500

    LC-validate-binary-search-tree [Runtime: 3 ms; Memory: 19.8 MB]

--- binary-tree/validate-binary-search-tree.py
+++ binary-tree/validate-binary-search-tree.py
@@ -6,30 +6,16 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        inorder = []
         def dfs(node):
-            if node.left and not node.right:
-                if node.left.val < node.val:
-                    return node.val, node.left.val, True
-                else:
-                    return node.left.val, node.val, False
-            if node.right and not node.left:
-                if node.right.val > node.val:
-                    return node.right.val, node.val, True
-                else:
-                    return node.val, node.right.val, False
-            if not node.left and not node.right:
-                return node.val, node.val, True
-            flag1 = True
-            flag2 = True
-            left_max, left_min, _ = dfs(node.left)
-            if left_max < node.val:
-                flag1 = True
-            else:
-                flag1 = False
-            right_max, right_min, _ = dfs(node.right)
-            if right_min > node.val:
-                flag2 = True
-            else:
-                flag2 = False
-            return right_max, left_min, flag1 and flag2
-        return dfs(root)[-1]
+            if not node:
+                return 
+            dfs(node.left)
+            inorder.append(node.val)
+            dfs(node.right)
+        dfs(root)
+        n = len(inorder)
+        for i in range(1, n):
+            if inorder[i] <= inorder[i-1]:
+                return False
+        return True
