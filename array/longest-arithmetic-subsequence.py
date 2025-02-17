fb1ce328aca236e2c6c8b2179353db9947539ef2
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-02-17 18:32:15 -0500

    LC-longest-arithmetic-subsequence [Runtime: N/A; Memory: N/A]

--- array/longest-arithmetic-subsequence.py
+++ array/longest-arithmetic-subsequence.py
@@ -5,10 +5,9 @@ class Solution:
         for i, b in enumerate(nums[1:], start=1):
             for j, a in enumerate(nums[:i]):
                 diff = b - a
-                if diff in dp:
-                    dp[diff] += 1
+                if (j, diff) in dp:
+                    dp[i, diff] += 1
                 else:
-                    dp[diff] = 2
-        dp = [(k, v) for k, v in dp.items()]
-        dp.sort(key=lambda x: x[1])
-        return dp[-1][-1]
+                    dp[i, diff] = 2
+        
+        return max(dp.values())
