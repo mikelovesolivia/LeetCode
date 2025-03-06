8dacf082ebd438fe52450d7118d2d44ab8149447
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-03-06 13:23:17 -0500

    LC-nearest-exit-from-entrance-in-maze [Runtime: N/A; Memory: N/A]

---
+++ matrix/nearest-exit-from-entrance-in-maze.py
@@ -0,0 +1,22 @@
+class Solution:
+    def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
+        m = len(maze)
+        n = len(maze[0])
+        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        queue = [(*entrance, 0)]
+        visited = [[False] * n for _ in range(m)]
+        while queue:
+            x, y, level = queue.pop(0)
+            visited[x][y] = True
+            for dx, dy in dirs:
+                nx, ny = x + dx, y + dy
+                if nx < 0 or nx >= m or ny < 0 or ny >= n:
+                    continue
+                if visited[nx][ny]:
+                    continue
+                if maze[nx][ny] == ".":
+                    if nx == 0 or nx == m - 1 or ny == 0 or ny == n - 1:
+                        return level + 1
+                    queue.append((nx, ny, level+1))
+        return -1
+            
