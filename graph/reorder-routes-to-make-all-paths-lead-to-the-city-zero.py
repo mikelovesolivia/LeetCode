27a881734ee8998f2b05c5f7ad3e929dcd4d72b4
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-03-06 20:21:45 -0500

    LC-reorder-routes-to-make-all-paths-lead-to-the-city-zero [Runtime: 278 ms; Memory: 65.5 MB]

--- graph/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ graph/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,6 +1,20 @@
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
         graph = [set() for _ in range(n)]
+        directed_graph = [set() for _ in range(n)]
         for u, v in connections:
-            graph[u].add(v)
-        return graph
+            graph[u].add((v, 1))
+            graph[v].add((u, -1))
+        visited = [False] * n
+        
+        def dfs(node):
+            if visited[node]:
+                return 0
+            count = 0
+            visited[node] = True
+            for neighbor, direction in graph[node]:
+                if not visited[neighbor] and direction == 1:
+                    count += 1
+                count += dfs(neighbor)
+            return count
+        return dfs(0)
