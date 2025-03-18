c207e9853d32976488ded7dee59d99abe9b0d7c4
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-03-18 19:21:49 -0400

    LC-target-sum [Runtime: 19 ms; Memory: 18 MB]

--- backtracking/target-sum.py
+++ backtracking/target-sum.py
@@ -4,10 +4,9 @@ class Solution:
         sum_ = sum(nums)
         if (sum_ + target) % 2 == 1 or abs(sum_) < abs(target):
             return 0
-        W = (sum_ + target) // 2
-        dp = [0] * (W+1)
+        dp = [0] * ((target + sum_) // 2 + 1)
         dp[0] = 1
         for i in range(n):
-            for j in range(W, nums[i]-1, -1):
+            for j in range((target + sum_) // 2, nums[i]-1, -1):
                 dp[j] += dp[j-nums[i]]
         return dp[-1]
