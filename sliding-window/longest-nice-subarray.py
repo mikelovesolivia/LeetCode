15875fe0f7f956fa33e978d5d6d01d6257ed6fd1
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-03-17 22:11:33 -0400

    LC-longest-nice-subarray [Runtime: N/A; Memory: N/A]

---
+++ sliding-window/longest-nice-subarray.py
@@ -0,0 +1,23 @@
+import copy
+class Solution:
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+        subarr = []
+        n = len(nums)
+        res = []
+
+        def isNice(num, arr):
+            for x in arr:
+                if num & x != 0:
+                    return False
+            return True 
+
+        for i in range(n):
+            if not subarr:
+                subarr.append(nums[i])
+            elif isNice(nums[i], subarr):
+                subarr.append(nums[i])
+            else:
+                subarr = [nums[i]]
+            if len(subarr) > len(res):
+                    res = subarr.copy()
+        return res
