737cd7202d50670abc5231af58c00ebb45fae513
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-03-18 17:45:31 -0400

    LC-guess-number-higher-or-lower-ii [Runtime: 1627 ms; Memory: 18.5 MB]

--- math/guess-number-higher-or-lower-ii.py
+++ math/guess-number-higher-or-lower-ii.py
@@ -1,9 +1,9 @@
 class Solution:
     def getMoneyAmount(self, n: int) -> int:
         dp = [[0 for _ in range(n+2)] for _ in range(n+2)]
-        for l in range(2, n+1):
+        for l in range(1, n):
             for i in range(1, n+1):
-                j = i + l - 1
+                j = i + l 
                 if j > n:
                     break
                 dp[i][j] = float('inf')
