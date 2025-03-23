ec64eab4699c3c080df342ffc4c25ef4769ee172
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-03-23 17:39:20 -0400

    LC-jump-game-vi [Runtime: 700 ms; Memory: 41.8 MB]

--- array/jump-game-vi.py
+++ array/jump-game-vi.py
@@ -3,7 +3,17 @@ class Solution:
         n = len(nums)
         dp = [-float('inf')] * n
         dp[0] = nums[0]
-        for i in range(n):
-            for j in range(i+1, min(i+k+1, n)):
-                dp[j] = max(dp[j], dp[i]+nums[j])
+        pq = [(-dp[0], 0)]
+        # for i in range(n):
+        #     for j in range(i+1, min(i+k+1, n)):
+        #         dp[j] = max(dp[j], dp[i]+nums[j])
+        for i in range(1, n):
+            max_dp, idx = heapq.heappop(pq)
+            max_dp = -max_dp
+            while idx < i - k:
+                max_dp, idx = heapq.heappop(pq)
+                max_dp = - max_dp
+            dp[i] = dp[idx] + nums[i]
+            heapq.heappush(pq, (-dp[i], i))
+            heapq.heappush(pq, (-dp[idx], idx))
         return dp[-1]
