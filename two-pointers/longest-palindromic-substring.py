88d88fb2904916f7ac42a6687b35fd9568f92a46
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-03-24 10:54:32 -0400

    LC-longest-palindromic-substring [Runtime: 2934 ms; Memory: 26.2 MB]

--- two-pointers/longest-palindromic-substring.py
+++ two-pointers/longest-palindromic-substring.py
@@ -1,23 +1,24 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
+        if n < 2:
+            return s
         dp = [[False for _ in range(n)] for _ in range(n)]
         # dp[i][j]: whether s[i...j] is a palinfromic string
-        res = ""
-        max_len = 0
-        for i in range(n-1,-1,-1):
-            for j in range(i, n):
-                if i==j:
-                    dp[i][j] = True
-                    if max_len<1:
-                        max_len=1
-                        res=s[i]
-                elif s[i] == s[j]:
-                    if j==i+1:
+        start, max_len = 0, 1
+        for i in range(n):
+            dp[i][i] = True
+        for j in range(1, n):
+            for i in range(j):
+                if s[i] == s[j]:
+                    if j - i <= 2:
                         dp[i][j] = True
                     else:
                         dp[i][j] = dp[i+1][j-1]
-                    if dp[i][j] and j-i+1 > max_len:
-                        max_len = j-i+1
-                        res=s[i:j+1]
-        return res
+                else:
+                    dp[i][j] = False
+                if dp[i][j] and j - i + 1 > max_len:
+                    start = i
+                    max_len = j - i + 1
+        return s[start:start+max_len]
+        
