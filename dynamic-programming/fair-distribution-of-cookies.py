197eb548fad334fdc9a69af3faeea3ec3613b8ee
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-04-01 17:59:06 -0400

    LC-fair-distribution-of-cookies [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/fair-distribution-of-cookies.py
@@ -0,0 +1,19 @@
+class Solution:
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+        n = len(cookies)
+
+        SUM = sum(cookies)
+        subsum = [0] * k
+        self.res = float('inf')
+    
+        def backtrack(i):
+            if i == n:
+                self.res = min(self.res, max(subsum))
+                return
+            for j in range(k):
+                subsum[j] += cookies[i]
+                backtrack(i+1)
+                subsum[j] -= cookies[i]
+                
+        backtrack(0) 
+        return self.res
