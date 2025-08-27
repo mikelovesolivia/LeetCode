5e81cf6880966b847c13ba702e1be1046ca29a96
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-08-27 10:13:06 -0700

    LC-best-time-to-buy-and-sell-stock [Runtime: 8 ms; Memory: 66 MB]

--- array/best-time-to-buy-and-sell-stock.js
+++ array/best-time-to-buy-and-sell-stock.js
@@ -5,8 +5,8 @@
 var maxProfit = function(prices) {
     let min_price = Infinity, max_profit = 0;
     for(let price of prices){
-        max_profit = Math.max(max_profit, price-min_price);
         min_price = Math.min(min_price, price);
+        max_profit = Math.max(max_profit, price-min_price);
     }
     return max_profit;
 };
