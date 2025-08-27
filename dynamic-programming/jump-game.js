54cbe59d35c218917b722e63ef8c1911d4fbef9f
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-08-27 09:48:03 -0700

    LC-jump-game [Runtime: 5 ms; Memory: 58.7 MB]

--- dynamic-programming/jump-game.js
+++ dynamic-programming/jump-game.js
@@ -3,6 +3,7 @@
  * @return {boolean}
  */
 var canJump = function(nums) {
+    if(nums.length==1 && nums[0]==0) return true;
     const dp = Array(nums.length).fill(0);
     dp[0] = nums[0];
     for(let i=1;i<nums.length;i++){
