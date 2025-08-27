5ac8a82f8a22e205e56e05a85185a4f85b14047d
Mike Lu <147356944+mikelovesolivia@users.noreply.github.com>
2025-08-27 09:45:22 -0700

    LC-jump-game [Runtime: N/A; Memory: N/A]

---
+++ greedy/jump-game.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    if(nums == [0]) return true;
+    const dp = Array(nums.length).fill(0);
+    dp[0] = nums[0];
+    for(let i=1;i<nums.length;i++){
+        dp[i] = Math.max(dp[i-1], i+nums[i]);
+    }
+    for(let i=0;i<nums.length;i++){
+        if(dp[i] == i) return false;
+        if(dp[i] >= nums.length-1) return true;
+    }
+};
